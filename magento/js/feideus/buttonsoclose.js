95b6293e36795fa40d0ea9b31c938490c6222044
Feideus <erwan.ulrich@gmail.com>
2016-07-19 17:32:59 +0300

    module pour enlever le bouton

--- magento/js/feideus/buttonsoclose.js
+++ magento/js/feideus/buttonsoclose.js
@@ -1,18 +1,7 @@
-function createButton(){
-
-    var button = document.createElement("input");
-    var LaDiv = document.getElementById("scriptos");
-    var Essai = document.getElementsByClassName("row");
-
-
-    Essai[0].style="background-color: red;";
-    alert(Essai[0]);
-    alert(LaDiv);
-    alert(button);
-    button.style="width:600px;height:600px;font-size:20px;background-color:red;"
-    button.type = "button";
-    button.value = "COUCOU";
-    button.onclick = "alert()";
-    LaDiv.appendChild(button);
-    
+function deleteButton(){
+       document.getElementById('soclozbutton').style.visibility='hidden';
 }
+
+setTimeout(deleteButton,1000);
+
+
